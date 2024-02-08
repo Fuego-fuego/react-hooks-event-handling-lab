8e08df51f17638f3b6c0de8ca2d4f302f1fa8cce
nathaniEL mujesia <nathanielmujesia@gmail.com>
2024-02-08 11:28:16 +0300

    Completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+    function handleChange (){
+
+    console.log("Entering password...");
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" name ="password" onChange ={handleChange}/>
+        </div>
     )
 }
 
